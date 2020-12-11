896537e9dcf5de98f543d2b1bc25912d7b94fbd0
Michael Iodice <miodice3@gmail.com>
2020-12-11 11:51:25 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,7 @@
-class Ride < ActiveRecord::Base
+class Ride < ApplicationRecord
+    belongs_to :user
+    belongs_to :attraction
+
+    # validates :user_id, presence: true
+    # validates :attraction_id, presence: true
 end
